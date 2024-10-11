8134215d5a372db4deabaf02ccdd370d7e4336eb
jyasukawa <jin.yasukawa123@gmail.com>
2024-10-11 19:18:20 +0900

    update: update task module

---
+++ server/src/task/dto/createTask.dto.ts
@@ -0,0 +1,8 @@
+import { IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class CreateTaskDto {
+    @IsString({ message: 'Task must be a string' }) //stringでないとエラー
+    @IsNotEmpty({ message: 'Task must not be empty' }) // 空文字や空白を許可しない
+    @MaxLength(255, { message: 'Task must not exceed 255 characters' }) // 255文字以内に制限
+    task: string;
+}
--- server/src/task/task.controller.ts
+++ server/src/task/task.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, ParseIntPipe, Patch } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { Task } from './entity/task.entity';
+import { CreateTaskDto } from './dto/createTask.dto';
+import { UpdateTaskDto } from './dto/updateTask.dto';
 
 @Controller('task')
-export class TaskController {}
+export class TaskController {
+    constructor(private readonly taskService: TaskService) {}
+
+    @Get()
+    async findAllFunc(): Promise<Task[]> {
+        return this.taskService.findAllTask();
+    }
+
+    @Get(':id')
+    async findOneFunc(@Param('id', ParseIntPipe) id: number): Promise<Task> {
+        return this.taskService.findOneTask(Number(id));
+    }
+
+    @Post()
+    async createTaskFunc(@Body() task: CreateTaskDto): Promise<Task> {
+        return this.taskService.createTask(task);
+    }
+
+    @Patch(':id')
+    async updateTaskFunc(@Param('id', ParseIntPipe) id: number, @Body() updatedTask: UpdateTaskDto): Promise<Task> {
+        return this.taskService.updateTask(id, updatedTask);
+    }
+
+    @Delete(':id')
+    async deleteTaskFunc(@Param('id', ParseIntPipe) id: number): Promise<void> {
+        await this.taskService.deleteTask(id);
+    }
+}
