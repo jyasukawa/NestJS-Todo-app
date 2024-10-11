d20394b4df2acf58509fb3f8cba5c6a4688aea1a
jyasukawa <jin.yasukawa123@gmail.com>
2024-10-11 16:37:55 +0900

    create: create task module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,6 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { TaslController } from './tasl/tasl.controller';
+import { TaskController } from './task/task.controller';
+import { TaskService } from './task/task.service';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
@@ -17,8 +21,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       synchronize: true,
     }),
     CatModule,
+    TaskModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, TaslController, TaskController],
+  providers: [AppService, TaskService],
 })
 export class AppModule {}
