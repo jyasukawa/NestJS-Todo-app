8c7d3cc6115e7c5bb2b949e818aab7e7f54d69c2
jyasukawa <jin.yasukawa123@gmail.com>
2024-10-15 10:48:21 +0900

    fix: add error handling

--- server/src/task/task.controller.ts
+++ server/src/task/task.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Param, Delete, ParseIntPipe, Patch } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Delete, ParseIntPipe, Patch, NotFoundException } from '@nestjs/common';
 import { TaskService } from './task.service';
 import { Task } from './entity/task.entity';
 import { CreateTaskDto } from './dto/createTask.dto';
@@ -10,22 +10,26 @@ export class TaskController {
 
     @Get()
     async findAllFunc(): Promise<Task[]> {
-        return this.taskService.findAllTask();
+        return await this.taskService.findAllTask();
     }
 
     @Get(':id')
     async findOneFunc(@Param('id', ParseIntPipe) id: number): Promise<Task> {
-        return this.taskService.findOneTask(Number(id));
+        return await this.taskService.findOneTask(id);
     }
 
     @Post()
     async createTaskFunc(@Body() task: CreateTaskDto): Promise<Task> {
-        return this.taskService.createTask(task);
+        return await this.taskService.createTask(task);
     }
 
     @Patch(':id')
     async updateTaskFunc(@Param('id', ParseIntPipe) id: number, @Body() updatedTask: UpdateTaskDto): Promise<Task> {
-        return this.taskService.updateTask(id, updatedTask);
+        const task = await this.taskService.findOneTask(id); // タスクが存在するか確認
+        if (!task) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+        return await this.taskService.updateTask(id, updatedTask);
     }
 
     @Delete(':id')
